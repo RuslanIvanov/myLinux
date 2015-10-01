708ce555ef868f89f3399b2a563f6cd501923928
Nasturtium <phonya@yandex.ru>
2015-10-02 00:02:12 +0300

    2015.10.01

--- 006/main.c
+++ 006/main.c
@@ -1,25 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
 
 int main(int arg,char **argv)	//arg - кол-во аргументов
 {				//argv - ук на аргумент
 
   if(arg != 3)   //если пользователь ввел не три аргумента
   {
-     printf("ERROR! check amount of arguments \n");
+     printf("ERROR: check amount of arguments! \n");
      exit(1);
   }
   
   int pid = atoi(argv[1]);	//код процесса
   int sign = atoi(argv[2]);	//код сигнала
-
-//printf("error: pid=%d ppid=%d \n",pid_code,ppid_code);
   
-printf("pid = %d sign = %d  \n",pid,sign);
-
-  
-
+  if(sign == SIGTERM )          //только 15 сигнал
+  {
+     if(kill(pid,sign) == -1)
+     {
+        printf("ERRROR: can't send signal! \n");
+        return 1;
+     }
+  }
 return 0; 
 }
 
