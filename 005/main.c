40aec64cc5f4bf1ed83fab4c0a851afee8093694
Nasturtium <phonya@yandex.ru>
2015-10-04 22:24:20 +0300

    2015.10.04

--- 005/main.c
+++ 005/main.c
@@ -2,41 +2,77 @@
 #include <string.h>
 #include <signal.h>	//sigaction()
 
-void function_handler(int s)
+sig_atomic_t sig_flag= 0;
+
+void handler_usr1(int s)
 {
-    fprintf(stderr,"\nGoodbye...\n");
-    if (raise(SIGTERM) == -1)
-        printf("ERROR: \n");
+    sig_flag = 1;
+}
+
+void handler_usr2(int s)
+{
+    sig_flag = 2;
 }
 
 int main(void)
 {
-    struct sigaction act;
+    struct sigaction act_usr1, act_usr2; //два действия пользователя
 
-    act.sa_handler = &function_handler;  //функция-обработчик сигнала
-    act.sa_flags = 0;                    //набор флагов
-    sigemptyset(&act.sa_mask);           //обнуляем
+    sigemptyset(&act_usr1.sa_mask);      //обнуляем
+    act_usr1.sa_handler = &handler_usr1; //функция-обработчик
+    act_usr1.sa_flags = 0;               //набор флагов
 
-    char command[BUFSIZ]="fffffff";
-   
-//    scanf("./%s",command);
-gets(command);
+    sigemptyset(&act_usr2.sa_mask);      //обнуляем
+    act_usr2.sa_handler = &handler_usr2; //функция-обработчик
+    act_usr2.sa_flags = 0;               //набор флагов
     
-printf("\n str20 ---> %s \n",command);
+    char command[BUFSIZ];	//команда-файл вводимая пользователем
+//    char dir[BUFSIZ];		//путь к файлу
+   
+    scanf("%s",command);
 
-    if(sigaction(SIGINT,&act,NULL) == -1)
+    
+//printf("\n str20 ---> %s \n",command);
+
+    if(sigaction(SIGINT,&act_usr1,NULL) == -1)
     {
-        fprintf(stderr, "ERROR: functoin sigaction() \n");
+        fprintf(stderr, "ERROR: sigaction() for act_usr1\n");
         return 1;
     }
 
+    if(sigaction(SIGTTIN,&act_usr2,NULL) == -1)
+    {
+        fprintf(stderr,"ERROR: sigation() for act_usr2 \n");   
+        return 1;
+    }
+
+
+printf("\n str20 ---> %s \n",command);
+
 printf("str28 \n");
 
+    while(1)
+    {
+        if(sig_flag == 1)
+        { 
+            fprintf(stderr,"\nGoodbye...\n");
+            if (raise(SIGTERM) == -1)
+                printf("ERROR: \n");
+            sig_flag = 0;
+        }
+        
+       if(sig_flag == 2)
+       {
+            printf(".....\n");
+       
+            sig_flag = 0;
+       }
+        
+        
+
+    } 
 
 
-    while(1);
-
-printf("str 34 \n");
 
     return 0; 
 }
