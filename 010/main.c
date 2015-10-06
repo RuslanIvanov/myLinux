55c7dbfc9ee2460bdd276d4434b8395b8e65533c
Nasturtium <phonya@yandex.ru>
2015-10-06 08:02:39 -0700

    2015.10.05

---
+++ 010/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{				//argv - ук на аргумент
+
+char buf[BUFSIZ];
+int i=0
+
+while(fgets(buf,BUFSIZ,stdin))
+{
+i++;
+fputs(buf);
+}
+
+printf("count = %d\n", i);
+
+
+return 0; 
+}
+
