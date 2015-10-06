0f45bbce35a43e728e0bd5c0808cba33097712c3
Nasturtium <phonya@yandex.ru>
2015-10-06 13:04:55 +0300

    2015.10.05

---
+++ 007/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <pthread.h>
+
+struct params{
+             double left;
+             double right;
+             //double *result;
+             };
+
+void * thread_func(void *arg)
+{
+
+}
+
+
+int main(void)
+{
+pthread_t thread;
+int arg;
+
+    int N = 0, n = 0;
+    struct params *p;
+
+    printf("Длина отрезка N = ");
+    scanf("%d",&N);
+    printf("Разбиений     n = ");
+    scanf("%d",&n);
+    printf("-> N = %d ; n = %d \n",N,n);
+
+    for(int i =1; i <= n; i++)
+    {
+
+        p = new struct params;
+        p->left = (N/(n-1)) * i;
+        p->right = (N/n) * i;  
+
+printf(" left = %f  right = %f \n",p->left,p->right);
+
+int result =pthread_create(&thread,NULL,&thread_func,&arg);
+
+
+delete p;
+}
+return 0; 
+}
+
