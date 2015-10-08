7d014907a7a88300ffdc290447d42ac951faa7d6
Nasturtium <phonya@yandex.ru>
2015-10-09 01:11:56 +0300

    no work

--- 007/main.c
+++ 007/main.c
@@ -2,47 +2,103 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <math.h>
 
 struct params{
              double left;
              double right;
-             //double *result;
+             int num;            //номер шага
+             double h;
+             double S;
              };
 
 void * thread_func(void *arg)
 {
-
+    double left,right;
+    struct params t = *(struct params*)arg;
+/*
+    left = sin_func.h * (sin_func.num - 1);
+    right = sin_func.h * sin_func.num;
+    double y_left = sin(left);
+    double y_right = sin(right);
+    sin_func.S = (y_left+y_right) * sin_func.h / 2;
+*/
+    t.S = 0;
+//printf("num = %d h = %f \n",t.num,t.h);
+   
+return 0;
 }
 
 
-int main(void)
+int main(int arg, char **argv)
 {
-pthread_t thread;
-int arg;
+    double S = 0;
+    double N = 0;
+    int n = 0;
 
-    int N = 0, n = 0;
-    struct params *p;
+    if(arg != 3)
+    {
+        printf("ERROR: неверное кол-во аргументов! \n");
+        printf("Длина отрезка N = ");
+        scanf("%lf",&N);
+        printf("Разбиений     n = ");
+        scanf("%d",&n);
+    }
+    else
+    {
+        N = atof(argv[1]);
+        n = atoi(argv[2]);
+    }
+printf("-> N = %lf ; n = %d \n",N,n);
+ 
+    pthread_t thread[n]; 
+    struct params trapeze[n];
 
-    printf("Длина отрезка N = ");
-    scanf("%d",&N);
-    printf("Разбиений     n = ");
-    scanf("%d",&n);
-    printf("-> N = %d ; n = %d \n",N,n);
-
+    //создаем потоки 
     for(int i =1; i <= n; i++)
     {
+	trapeze[i].num = i;
+	trapeze[i].h = N/n;	//одинаков для всех
+	if(pthread_create(&thread[i],NULL,&thread_func,&trapeze[i]) != 0)
+	{
+        	fprintf(stderr,"ERROR: pthread_create() \n");
+		return 1;
+	}
+printf(" i = %d | num = %d  h = %f \n",i,trapeze[i].num,trapeze[i].h);
+    }
 
-        p = new struct params;
-        p->left = (N/n) * (i-1);
-        p->right = (N/n) * i;  
+printf("MAIN FUNCTION!!!!!!!!!!! \n");
 
-printf(" left = %f  right = %f \n",p->left,p->right);
+int status;
+int res;
+    for(int i=1;i<=n;i++)
+    {
+printf("str77! \n");    	
+	status =pthread_join(thread[i],(void **)&res);
+printf("str79 status = %d ! \n",status);    	
+	if(status != 0)
+    	{
+		fprintf(stderr,"ERROR: join() \n");
+		return 1;
+	}
 
-//int result =pthread_create(&thread,NULL,&thread_func,&arg);
+printf("str84! \n");
 
+    }
+//double nnn = pthread_join(thread,(void **) &S_func);
+//     if (pthread_join(thread,NULL)!=0)
+   //  {
+//         fprintf(stderr,"ERROR: join function\n");
+//         return 1;
+    // }
+//printf("2 h = %f i = %d S_func = %f nnnnnnnn=%f \n",sin.h,sin.num,S_func,nnn);
 
-delete p;
+for(int i=1;i<=n;i++)
+{
+printf("trapeze[%d].S = %f \n",i,trapeze[i].S);
 }
+
+
 return 0; 
 }
-
--- 009/main.c
+++ 009/main.c
@@ -2,10 +2,9 @@
 #include<stdlib.h>
 #include<string.h>
 
-
-
 int main(int arg,char **argv)	//arg - кол-во аргументов
 {				//argv - ук на аргумент
+  char cmd[BUFSIZ];
   char buf[BUFSIZ];
   FILE *my_pipe;
 
@@ -14,7 +13,29 @@ int main(int arg,char **argv)	//arg - кол-во аргументов
      printf("ERROR! number arg is NULL! \n");
      exit(1);
   }
+  else
+  {
+     strcpy(cmd,argv[1]);
+printf("arg = %d cmd = %s \n",arg,cmd);     
+     if(arg>2)	// если аргументов больше одного
+     {int i = 2;
+	while(i<=arg)
+	{
+		strcat(cmd," ");
+		strcat(cmd,argv[i]);
+i++;
+	}
+//	for(int i=2;i<=arg;i++)
+//	{
+//strcat(cmd," m");
+//strcat(cmd,argv[i]);
+//sprintf(cmd,"%s",argv[i]);
+//	}
+     }
+  }
 
+
+printf("%d cmd = %s \n",arg,cmd);     
 
   
   if (! (my_pipe = popen(argv[1],"r")))
@@ -22,28 +43,18 @@ int main(int arg,char **argv)	//arg - кол-во аргументов
 
   while (fgets(buf,sizeof(buf),my_pipe))
   {
-    printf("buf = %s \n",buf);
     int N = strlen(buf);
-    int i;
-    for(i=0;i<N;i++)
+    char my_buf[N];
+    for(int i=0;i<N/2;i++)
     {
-    char p;
-   p = buf[i];
- buf[i]=buf[N-i-1];
- buf[N-i-1]=p;
-printf("%s",&buf[i]);
-
-}
-
-    printf("buf = %s \n",buf);
-
-
-}
-
-
+        char p;
+        p = buf[i];
+        buf[i]=buf[N-i-1];
+        buf[N-i-1]=p;
+    }
+printf("%s",buf);
+  }
+printf("\n");
 pclose(my_pipe);
-
-
 return 0; 
 }
-
