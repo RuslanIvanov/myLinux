589c6567d41319b66d7995c2e93f0c3239da5cc0
Nasturtium <phonya@yandex.ru>
2015-10-11 17:03:26 +0300

    tie-tie 2015.10.11

--- 010/main.c
+++ 010/main.c
@@ -2,23 +2,44 @@
 #include<stdlib.h>
 #include<string.h>
 
-int main()
-{				//argv - ук на аргумент
-
-char buf[BUFSIZ];
-int i=0;
-
-while(fgets(buf,BUFSIZ,stdin))
+int main(int arg, char **argv)
 {
-i++;
-fputs(buf,stdin);
+    FILE *my_file;
 
-printf("buf = %s \n",buf);
-}
+    char buf[BUFSIZ];
+    char *p_buf = buf;
 
-printf("count = %d\n", i);
+    int  num_find = 0; 
 
+    if(arg != 3)
+    {
+        printf("ERROR: check amount of arguments! \n");
+        exit(1);
+    }
 
+    my_file = fopen(argv[2],"w");	//создаем файл для записи
+    if(!my_file)
+    {
+        printf("ERROR: can`t open/create file! \n");
+        exit(1);
+    }
+
+    while(fgets(buf,BUFSIZ,stdin))	//читаем строку из потока в stdin
+    {
+       fputs(buf, stdout);		//записываем строку в поток stdout
+       if(strstr(p_buf,argv[1]))        //ищем в строке
+       {
+           num_find++;
+           //fprintf(my_file,"%s\n",p_buf);
+       }
+    }
+    //printf("buf = %s num = %d \n",buf,num_find);
+    if(num_find)
+        fprintf(my_file,"The word '%s' are found %d ps\n",argv[1],num_find);
+    else
+        fprintf(my_file,"The word '%s' isn't found\n",argv[1]);
+
+    fclose(my_file);
+  
 return 0; 
 }
-
