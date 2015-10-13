131915b1605d8ccd5a6567b75bd7668bb94567b2
Nasturtium <phonya@yandex.ru>
2015-10-13 12:41:00 +0300

    name FIFO

--- 011/main.c
+++ 011/main.c
@@ -6,39 +6,45 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-//#define FIFO_IN		"pipeinllll"
-//#define FIFO_OUT	"pipeout"
-
 int main(int arg,char **argv)	//arg - кол-во аргументов
 {				//argv - ук на аргумент
+  //FILE *fifo_in;
   char pipein[BUFSIZ];
-  char pipeout[BUFSIZ];
+//  char pipeout[BUFSIZ];
 
-  int fifo_in,fifo_out;
-  pid_t pid_in,pid_out;
+//  int fifo_in,fifo_out;
+//  pid_t pid_in,pid_out;
 
   char buf[BUFSIZ];	//буфер
-  char *p_buf=buf;
+  char *p_buf;
 
   if(arg == 1)   //если пользователь ничего не ввел
   {
      printf("ERROR: number arg is NULL! \n");
+     printf(" ./mychat fifoin fifoout \n");
      exit(1);
   }
   else
   {
-      sprintf(pipein,"/tmp/%s",argv[1]);
-      sprintf(pipeout,"/tmp/%s",argv[2]);
-
-//      unlink(pipein);
-//      unlink(pipeout);
-
-      if(mkfifo(pipein, O_RDWR) == -1)    //создание FIFO
+      sprintf(pipein,"%s",argv[1]);
+//      sprintf(pipeout,"/tmp/%s",argv[2]);
+//mkfifo(topin,  O_RDWR|S_IFIFO|S_IRWXU|S_IRWXG|S_IRWXO);
+      if(mkfifo(pipein, O_RDONLY) == -1)    //создание FIFO_IN
       {
           fprintf(stderr,"ERROR: Can`t create fifo \n");
-          exit (1);
+          return (1);
+      }
+    
+      int fifo_in = open(pipein,O_RDONLY | O_NONBLOCK);
+      if(fifo_in <0)
+      {
+          fprintf(stderr,"ERROR: Can`t open FIFO %s \n",pipein);
+          exit(1);
       }
 
+close(fifo_in);
+
+/*
       pid_in=fork();	//процесс читает
       if(pid_in == -1)
       {
@@ -60,11 +66,11 @@ int main(int arg,char **argv)	//arg - кол-во аргументов
           else
               printf(" %s \n",buf);
           close(fifo_in);
-/*    while(1)
-      {
-      scanf("%s",buf);
-      fscanf(fifo,"%s",p_buf);  }
-*/
+//    while(1)
+//    {
+ //     scanf("%s",buf);
+ //     fscanf(fifo,"%s",p_buf);  }
+
        }
 
        if(mkfifo(pipeout, O_RDWR) == -1)    //создание FIFO
@@ -98,7 +104,7 @@ int main(int arg,char **argv)	//arg - кол-во аргументов
            }
            close(fifo_out);
        }
-
+*/
 
   }   
 return 0; 
