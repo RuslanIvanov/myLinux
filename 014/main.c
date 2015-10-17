d82f0c1b82f2cacfd0a0d99eb641e22de46bb817
Nasturtium <phonya@yandex.ru>
2015-10-17 03:26:20 +0300

    no =(

---
+++ 014/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>	//socket
+#include <sys/socket.h>	//socket
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+int sock;
+int myport;
+
+void * func_thread(void *arg)
+{
+	struct sockaddr_in addr;
+	char buf[BUFSIZ];
+
+	memset(&addr, 0, sizeof(addr));
+	//определяем параметры соединения
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(myport);			//отправляем
+	addr.sin_addr.s_addr=htonl(INADDR_BROADCAST);   //ВСЕМ!
+
+	while(1)
+	{
+		if(scanf("%s",buf) > 0)
+		{
+printf("послыаем buf = %s port = %d sock = %d \n",buf,myport,sock);		
+               		int send = sendto(sock,buf,strlen(buf),0,(struct sockaddr *)&addr,sizeof(addr));
+			if(send == -1)
+				fprintf(stderr,"ERROR: sendto() thread \n");
+		}
+	}
+}
+
+int main(int arg,char **argv)
+{
+	pthread_t thread;
+	char buf[BUFSIZ];
+
+	struct sockaddr_in addr;
+
+	if(arg == 1)   //если пользователь не ввел аргументы
+	{
+		fprintf(stderr,"ERROR: number arg is NULL! \n");
+		printf("1 copy: ./broadcast port \n");
+//		printf("2 copy: ./broadcast port \n");
+		exit(1);
+  	}
+  	else
+  	{
+		myport = atoi(argv[1]);
+
+printf("myport = %d \n",myport);
+
+		//создаем сокет
+ 		sock = socket(AF_INET,SOCK_DGRAM,0);
+		if(sock == -1)
+		{
+			fprintf(stderr,"ERROR: socket() error \n");
+			exit(1);
+		}
+        	
+		memset(&addr, 0, sizeof(sockaddr_in));		
+		//определяем параметры соединения
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(myport);
+		addr.sin_addr.s_addr = htonl(INADDR_ANY);	//слушаем ВСЕХ!!!
+		//назначаем адрес
+		int b = bind(sock,(struct sockaddr*)&addr,sizeof(addr));
+		if(b == -1)
+		{
+			fprintf(stderr,"ERROR: bind() \n");
+			exit(1);
+		}
+		//запускаем потоковую фукцию
+		if(pthread_create(&thread,NULL,&func_thread,NULL) != 0)    //&my_fifo
+        	{
+			fprintf(stderr,"ERROR: pthread_create() ! \n");
+			exit(1);
+        	}
+
+		while(1)
+		{
+printf("recv >\n");
+			memset(&buf,0,sizeof(buf));
+			int recv = recvfrom(sock,buf,sizeof(buf),0,NULL,NULL);
+			if(recv == -1)
+				fprintf(stderr,"ERROR: recvfrom() \n");
+			printf("%s\n",buf);
+		}
+	}	
+return 0; 
+}
