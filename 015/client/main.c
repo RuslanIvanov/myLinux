e7ace707c4207059cb81d85682ab90e12fd8f5e8
Nasturtium <phonya@yandex.ru>
2015-10-21 01:07:55 +0300

    TCP server/client

--- 015/client/main.c
+++ 015/client/main.c
@@ -7,6 +7,14 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+void menu()
+{
+     printf("\nmenu:\n");
+	printf("   'msgNx' - вывод последних сообщений (где х - кол-во сообщений) \n");
+	printf("   'exit' - отключение от сервера \n\n");
+}
+
+
 int main(int arg,char **argv)
 {
 	if(arg != 3)
@@ -17,14 +25,16 @@ int main(int arg,char **argv)
   	}
   	else
   	{
-        	char message[BUFSIZ];
+        
+		menu();
+		char message[BUFSIZ];
 		char message_serv[BUFSIZ];
 
 		//создаем сокет
  		int sock = socket(AF_INET,SOCK_STREAM,0);
 		if(sock == -1)
 		{
-			fprintf(stderr,"ERROR: create socket() \n");
+			fprintf(stderr,"error create socket() \n");
 			exit(1);
 		}
         	
@@ -40,12 +50,12 @@ int main(int arg,char **argv)
 		addr.sin_port = htons(my_port);			//порт
 //		addr.sin_addr.s_addr = inet_addr(hostinfo);	//
 		addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);	//для проверки на одной машине
-int cnt = 0;
+		int cnt = 0;
 		//установка соединения
 		while(connect(sock,(struct sockaddr*)&addr,sizeof(addr))<0)
 		{
 			if(cnt == 0)
-				printf("\n отсутствует подключение к серверу . .\n");
+				printf("\nподключение к серверу . .\n");
 			cnt=1;
 		}
 
@@ -55,22 +65,23 @@ printf(" + Подключен к серверу \n");
 		{
 			memset(&message,0,sizeof(message));
 
-			printf("              -> your message: ");
+			printf(" -> ");
 			scanf("%s",message);
 
 			//отправляем сообщение на сервер
 			send(sock,message,sizeof(message),0);
-
-                        if(strcmp(message,"exit") == 0) //ввел exit - закрыть сокет и выйти
+			//если сообщение - exit
+                        if(strcmp(message,"exit") == 0)
                         {
-                                printf("отключение от сервера \n");
+                                printf("отключение от сервера . . . \n");
                                 close(sock);
-                                sleep(2);
+				sleep(1);
+				printf("- Отключен \n");
                                 exit(1);
                         }
 
 			int bytes = recv(sock,message_serv,sizeof(message_serv),0);
-			printf("echo from server -> %s \n",message_serv);
+			printf("%s",message_serv);
 		}
 
 
