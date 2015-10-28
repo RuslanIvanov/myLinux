c3e3dc19feb21448a4aaf442d0fb5d6759ba7de7
Nasturtium <phonya@yandex.ru>
2015-10-29 00:46:59 +0300

    statserver

--- 016/main.c
+++ 016/main.c
@@ -9,6 +9,10 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
+#define HTML_text	200
+#define HTML_image	201
+#define HTML_error	404
+	
 int main(int arg,char **argv)
 {			  
 	if(arg != 2)
@@ -54,79 +58,73 @@ int main(int arg,char **argv)
 
 	while(1)
 	{
-	//запрос на соединение
-	int sock_accept = accept(sock,(sockaddr*)(&fromaddr), &ln);	//новый сокет для работы
-	if(sock_accept <= 0)
-	{
-		printf("error: accept() %d\n",sock_accept);
-		break;
-	}
-	//ожидаем сообщение от клиента-размер данных
-	int bytes = recv(sock_accept,message,sizeof(message),0);
-	if(bytes < 0)
-	{	//ошибка получения данных
-		printf("error: recv() failed\n");
-		close(sock_accept);
-//	break;
-	}
+		//запрос на соединение
+		int sock_accept = accept(sock,(sockaddr*)(&fromaddr), &ln);	//новый сокет для работы
+		if(sock_accept <= 0){
+			printf("error: accept() %d\n",sock_accept);
+			break;
+		}
+		//ожидаем сообщение от клиента-размер данных
+		int bytes = recv(sock_accept,message,sizeof(message),0);
+		if(bytes < 0){
+			//ошибка получения данных
+			printf("error: recv() failed\n");
+			close(sock_accept);
+			//break;
+		}
 	/*
 	else if(bytes == 0)
 	{	//соединение закрыто клиентом
 		printf("connection closed\n");		
 	}*/
-//	else if(bytes >= 0)
+	//else if(bytes >= 0)
 	{	//полученные данные
 		message[bytes] = '\0';	//метка конца строки
 		//формируем ответ
-printf("запрос:\n%s\n",message);
-int code_ans = 0;
+		printf("запрос:\n%s\n",message);
+		int code_ans = 0;
 
-	char *html = strstr(message,"index.html");
-	char *jpg  = strstr(message,"img");
-	if (html == NULL)
-		code_ans = 404;
-	else code_ans = 200;
+		if(strstr(message,"GET /index.html"))
+			code_ans = HTML_text;
+		else if (strstr(message,"GET /img.jpeg"))
+			code_ans = HTML_image;
+		else code_ans = HTML_error;
+		//printf("code = %d \n",code_ans);
+		
+		char head[BUFSIZ];
+		char *tmp = head;
+		int len =0;
 
-printf("code = %d \n",code_ans);
-char head[1024];
-char *tmp = head;
-int len =0;
-
-		if(code_ans == 200)
+		if(code_ans == HTML_image)
 		{
-			if(jpg != NULL)
-                 	{
-				char buff[1024];
+			char buff[1024];
  
-                         FILE* fd_htm = fopen("img.jpg", "r");
-                         if(fd_htm == 0)
+                         FILE* fd_jpg = fopen("img.jpeg", "rb");
+                         if(fd_jpg == 0)
                                  printf("error open()");
                          //размер файла
-                         fseek(fd_htm,0,SEEK_END);
-                         long int size_htm = ftell(fd_htm);
+                         fseek(fd_jpg,0,SEEK_END);
+                         long int size_jpg = ftell(fd_jpg);
                          //считываем данные из файла
-                         fseek(fd_htm, 0, SEEK_SET);
-                         int r = fread(tmp,size_htm,1,fd_htm);
-                         //формируем ответ,считанный из файла
+                         fseek(fd_jpg, 0, SEEK_SET);
+                         int r_jpg = fread(tmp,size_jpg,1,fd_jpg);
+			 //формируем ответ,считанный из файла
                          strcpy(uploadBuff,"HTTP/1.1 200 OK\r\n");
                          strcat(uploadBuff,"Connection: keep-alive\r\n");
-                         strcat(uploadBuff,"Content-Type: text/html; charset=UTF-8\r\n");
+                         strcat(uploadBuff,"Content-Type: image/jpeg\r\n");
                          strcat(uploadBuff,"Keep-Alive: timeout=5,max=97\r\n");
                          strcat(uploadBuff,"\r\n");
                          strcat(uploadBuff,tmp);
                          //рассчитываем размер сообщения
                          len = strlen("HTTP/1.1 200 OK\r\n");
                          len +=strlen("Connection: keep-alive\r\n");
-                         len +=strlen("Content-Type: text/html; charset=UTF-8\r\n");
+                         len +=strlen("Content-Type: image/jpeg\r\n");
                          len +=strlen("Keep-Alive: timeout=5,max=97\r\n");
                          len +=strlen("\r\n");
-                         len +=size_htm;
- //printf("------\n\nbuff=%s \n",uploadBuff);
-                         fclose(fd_htm);
+                         len +=size_jpg;
+                         fclose(fd_jpg);
 		}
-
-		else
-			{
+		else if (code_ans == HTML_text){
 			char buff[1024];
 
 			FILE* fd_htm = fopen("index.html", "r");
@@ -152,12 +150,9 @@ int len =0;
 	                len +=strlen("Keep-Alive: timeout=5,max=97\r\n");
         	        len +=strlen("\r\n");
 			len +=size_htm;
-//printf("------\n\nbuff=%s \n",uploadBuff);
 			fclose(fd_htm);
-			}
 		}
-        	else if(code_ans == 404)
-        	{
+        	else	{
 			//ответ - страница не найдена
                 	strcpy(uploadBuff,"HTTP/1.1 404 Not Found\r\n");
 	                strcat(uploadBuff,"Connection: keep-alive\r\n");
